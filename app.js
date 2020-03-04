73363cf6eaf26ef3170710c9f25801ccabe3e27b
elainefigueiredo87 <61721963+elainefigueiredo87@users.noreply.github.com>
2020-03-04 10:34:13 -0300

    conversor de temperatura

---
+++ app.js
@@ -0,0 +1,8 @@
+//perguntar a temperatura em celsius(ºC)
+var temperature = prompt("Qual a temperatura em Celsius?")
+
+//converter a temperatura para Farenheit (ºF)
+var convertToFarenheit = (temperature*1.8) + 32;
+
+//mostrar o resultado no console
+console.log(" A temperatura em Farenheit é: " + convertToFarenheit);
